12f50d12605e497f29b5506be6b20572c8c6cbbf
legshampoo <daniel.c.baker@gmail.com>
2018-03-15 23:13:46 -0400

    akj

--- server.js
+++ server.js
@@ -113,7 +113,7 @@ const server = app.listen(app.get('port'), '0.0.0.0', function () {
   database.init();
   io.listen(server);
 
-  socketServer.init(io);
+  // socketServer.init(io);
   // reddit_bot.init(io);
   // gdax_market_feed.init(io);  //also kicks off the trading bot
   // gdax_bot.init(io);
